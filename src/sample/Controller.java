156a2ef300271f365049009cc7438ab44e0f5334
W0207 <710495718@qq.com>
2020-04-28 23:44:54 +0800

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -38,65 +38,10 @@ public class Controller implements Initializable{
 
     }
 
-    public void Button_Connect(javafx.event.ActionEvent actionEvent)throws IOException,ClassNotFoundException {
-        client.connect("localhost", 2221);
-
-        String init="D:/server";
-
-        String x,y,z;
-
+    public void Print_List(String x)
+    {
+        String y,z;
         int m,p,q;
-
-
-        ResponseBody a = client.switch1(init);
-
-        ResponseBody b = client.switch2(init);
-
-        Map<String, Boolean> c = client.switch3(init);
-
-        x=c.toString();
-        m=x.length();
-        y=x.substring(1,m-1);
-        p=y.indexOf("=");
-        if(p!=-1) {
-              z = y.substring(0, p);
-              show_list.appendText(z);
-              show_list.appendText("\n");
-              int r = z.length();
-              y = y.substring(r+1,y.length());
-        }
-        for(m=0;m<y.length();m++)
-        {
-           p=y.indexOf(" ");
-           q=y.indexOf("=");
-           if(p!=-1&&q!=-1) {
-               z = y.substring(p+1, q);
-               show_list.appendText(z);
-               show_list.appendText("\n");
-               y=y.substring(q+1,y.length());
-           }
-           if(p==-1||q==-1)
-               break;
-        }
-
-        show_response.appendText(a.toString());
-        show_response.appendText("\n");
-        show_response.appendText(b.toString());
-        show_response.appendText("\n");
-
-    }
-
-    public void Button_Confirm(javafx.event.ActionEvent actionEvent)throws IOException,ClassNotFoundException {
-        String catalog_address=catalog.getText().toString();
-
-        String x,y,z;
-        int m,p,q;
-
-        ResponseBody a = client.switch1(catalog_address);  //切换目录
-        ResponseBody b = client.switch2(catalog_address);  //输出当前目录
-        Map<String, Boolean> c = client.switch3(catalog_address);  //打印列表
-        show_list.setText("");
-        x=c.toString();
         m=x.length();
         y=x.substring(1,m-1);
         p=y.indexOf("=");
@@ -120,6 +65,34 @@ public class Controller implements Initializable{
             if(p==-1||q==-1)
                 break;
         }
+    }
+    public void Button_Connect(javafx.event.ActionEvent actionEvent)throws IOException,ClassNotFoundException {
+        client.connect("localhost", 2221);
+        String init="D:/server";
+
+        ResponseBody a = client.switch1(init);//切换目录
+        ResponseBody b = client.switch2(init);//输出当前目录
+        Map<String, Boolean> c = client.switch3(init);//打印列表
+
+        Print_List(c.toString());
+
+        show_response.appendText(a.toString());
+        show_response.appendText("\n");
+        show_response.appendText(b.toString());
+        show_response.appendText("\n");
+
+    }
+
+    public void Button_Confirm(javafx.event.ActionEvent actionEvent)throws IOException,ClassNotFoundException {
+        String catalog_address=catalog.getText().toString();
+
+        ResponseBody a = client.switch1(catalog_address);  //切换目录
+        ResponseBody b = client.switch2(catalog_address);  //输出当前目录
+        Map<String, Boolean> c = client.switch3(catalog_address);  //打印列表
+        show_list.setText("");
+
+        Print_List(c.toString());
+
         show_response.appendText(a.toString());
         show_response.appendText("\n");
         show_response.appendText(b.toString());
